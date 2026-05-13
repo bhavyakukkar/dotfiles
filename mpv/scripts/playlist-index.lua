8dade2d0e24cce4eae9b4b644224c3b6bbfaad29
Bhavya Kukkar <bhavyakukkar@gmail.com>
2026-05-13 21:17:17 +0530

    feat(mpv script): fancier

--- mpv/scripts/playlist-index.lua
+++ mpv/scripts/playlist-index.lua
@@ -1,5 +1,21 @@
-function on_index_show(name, value)
-  if value >= 0 then print('--------> ' .. value) end
+local count = nil
+local index = nil
+
+local function on_index_show()
+  if index and index >= 0 and count and count >= 0 and index < count then
+    print('----------------------------------------------> \27[1m[\27[0m ' ..
+      index .. '. ' ..
+      '\27[35;1m↓' .. (index + 1) .. '\27[0m ' ..
+      '\27[32;1m↑' .. (count - index) .. '\27[0m ' ..
+      '\27[33;1m/' .. count .. '\27[0m \27[1m]\27[0m')
+  end
 end
 
-mp.observe_property("playlist-pos", "number", on_index_show)
+mp.observe_property("playlist-count", "number", function(name, value)
+  count = value
+  on_index_show()
+end)
+mp.observe_property("playlist-pos", "number", function(name, value)
+  index = value
+  on_index_show()
+end)
